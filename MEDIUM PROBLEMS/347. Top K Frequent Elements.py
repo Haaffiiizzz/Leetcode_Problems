e04766248ac11f4f95f826326b2a9a3e2ddc378f
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-02-15 10:16:45 -0700

    TOP K FREQUENT ELEMENTS SOLUTION

---
+++ MEDIUM PROBLEMS/347. Top K Frequent Elements.py
@@ -0,0 +1,7 @@
+nums = [1,1,1,2,2,3]
+k = 2     # for test
+numCount = []
+for num in set(nums):
+    numCount.append([num, nums.count(num)])
+numCount.sort(reverse=True, key = lambda x: x[1])   
+print([i[0] for i in numCount[0:k] ])      #return instead
