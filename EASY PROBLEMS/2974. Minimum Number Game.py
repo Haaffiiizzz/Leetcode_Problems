fccb8886a697fa319b614d4dfbba3a92afd2c0c0
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-02-21 23:16:49 -0700

    done

---
+++ EASY PROBLEMS/2974. Minimum Number Game.py
@@ -0,0 +1,9 @@
+nums = [5,4,2,3]    # for test
+nums.sort()
+arr = []
+alice = [i for i in nums[::2]]
+bob = [i for i in nums[1::2]]
+for i in range(len(bob)):
+    arr.append(bob[i])
+    arr.append(alice[i])
+print(arr)   # append instead    
