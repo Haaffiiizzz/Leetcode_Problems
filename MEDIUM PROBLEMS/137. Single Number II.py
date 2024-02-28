b9c26218a3ca343b91fc06befc259bad623f9d1f
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-02-27 23:24:25 -0700

    fixed the time complexity. runs way faster now

--- MEDIUM PROBLEMS/137. Single Number II.py
+++ MEDIUM PROBLEMS/137. Single Number II.py
@@ -1,4 +1,11 @@
 nums = [2, 2, 3, 2, 4, 4, 4]    # for test
+countDict = {}
 for n in nums:
-    if nums.count(n) ==1:
-        print(n)     # return instead
+    if n in countDict:
+        countDict[n] += 1  
+    else:
+        countDict[n] = 1
+            
+for num, count in countDict.items():
+    if count == 1:
+        print(num)    # return instead 
