277def410af982f4d3d521098583f6b5c0f3c44c
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-03-01 23:12:45 -0700

    DOESNT WORK QUITE YET

---
+++ EASY PROBLEMS/13. Roman to Integer.py
@@ -0,0 +1,11 @@
+s = "MCMXCIV"
+Dict = {"I" : 1, "V": 5, "X" : 10, "L":50, "C": 100, "D": 500, "M": 1000 }
+
+first = []
+answer = []
+for i in range(len(s)):
+    if Dict.get(s[i]) > Dict.get(s[i+1]):
+        answer.append(Dict.get(s[i]))
+    else:
+        if Dict.get(s[i]) < Dict.get(s[i+1]):
+            answer.append(Dict.get(s[i+1])- Dict.get(s[i]))
