5a05427a825e6216d7a32d4c23b5c13c657e2626
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-04-11 23:43:09 -0600

    .

--- EASY PROBLEMS/13. Roman to Integer.py
+++ EASY PROBLEMS/13. Roman to Integer.py
@@ -1,7 +1,7 @@
 s = "MCMXCIV"
 Dict = {"I" : 1, "V": 5, "X" : 10, "L":50, "C": 100, "D": 500, "M": 1000 }
 
-first = []
+firstt = []
 answer = []
 for i in range(len(s)):
     if Dict.get(s[i]) > Dict.get(s[i+1]):
