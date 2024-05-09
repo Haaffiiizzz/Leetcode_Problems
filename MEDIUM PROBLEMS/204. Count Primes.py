b63d24eb85a6690a92cfaed99e8e94001956c542
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-05-08 21:46:44 -0600

    works but slow.

---
+++ MEDIUM PROBLEMS/204. Count Primes.py
@@ -0,0 +1,17 @@
+n = 10
+
+def prime(num):
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, num, 2):
+        if num % i == 0:
+            return False
+    return True
+
+primes = []
+for i in range(2, n):
+    if prime(i):
+        primes.append(i)
+print(len(primes))
