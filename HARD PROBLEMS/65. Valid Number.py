dd13c4ce7283981cdfd0989efb59383e07f87849
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-06-16 22:51:53 -0600

    .

--- HARD PROBLEMS/65. Valid Number.py
+++ HARD PROBLEMS/65. Valid Number.py
@@ -1,12 +1,20 @@
 if s.isdigit():
             return True
         
-        if not s[0].isdigit() and s[0] != "-" and s[0] != "+":
+        if not s[0].isdigit() and s[0] != "-" and s[0] != "+" and s[0] != ".":
             return False
         
         if "+" in s and "-" in s:
             return False
         
+        if "e" in s or "E" in s:
+            if "." in s[s.index(e):-1]:
+                return False
+            if s[s.index(e):-1]
         
+        if not s.isdigit() and len(s) == 1:
+            return False
+        if len(set(s)) == 1 and not s.isdigit():
+            return False
         
         return True
