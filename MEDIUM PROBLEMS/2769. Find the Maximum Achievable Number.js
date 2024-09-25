f46e998b7fb0e259aa0fddcad815c98a55949766
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-09-24 20:53:04 -0500

    More javascript

---
+++ MEDIUM PROBLEMS/2769. Find the Maximum Achievable Number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    let achievable = num + (2 * t)
+    return achievable 
+    
+};
