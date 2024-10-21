a86c35c0d392d319005890fa39196d192fc36963
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-10-21 14:58:51 -0500

    works but slow. doestn pass all tests

---
+++ MEDIUM PROBLEMS/5. Longest Palindromic Substring.py
@@ -0,0 +1,24 @@
+def longestPalindrome(s: str) -> str:
+        if len(s) == 1:
+            return s
+        if len(set(s)) == 1:
+            return s
+        palindromes = []
+        def isPalindrome(word):
+            reversedWord = word[::-1]
+            return word == reversedWord
+        
+        
+        def getSubstrings(word):
+            for i in range(0, len(word)):
+                for j in range(1, len(word)+1):
+                    newWord = word[i:j]
+                    print("newword", newWord)
+                    if isPalindrome(newWord):
+                        print("palindrome", newWord)
+                        palindromes.append(newWord)
+                        
+            
+        getSubstrings(s)
+        return sorted(palindromes, key=len, reverse=True)[0]
+print(longestPalindrome("abb"))
