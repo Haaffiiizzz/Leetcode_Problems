1cee735b9f6a09074d61df737b64441a23eca73d
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-10-30 14:33:37 -0500

    Works

--- EASY PROBLEMS/13. Roman to Integer.py
+++ EASY PROBLEMS/13. Roman to Integer.py
@@ -1,11 +1,18 @@
-s = "MCMXCIV"
-Dict = {"I" : 1, "V": 5, "X" : 10, "L":50, "C": 100, "D": 500, "M": 1000 }
-
-firstt = []
-answer = []
-for i in range(len(s)):
-    if Dict.get(s[i]) > Dict.get(s[i+1]):
-        answer.append(Dict.get(s[i]))
+s = "III"
+length = len(s)
+numerals = {"I" : 1, "V": 5, "X" : 10, "L":50, "C": 100, "D": 500, "M": 1000 }
+summed = 0
+i = 0
+while i < length:
+    if i == length - 1:
+        summed += numerals[s[i]]
+        i+= 1
+    elif numerals[s[i]] >= numerals[s[i+1]]:
+        summed += numerals[s[i]]
+        i += 1
     else:
-        if Dict.get(s[i]) < Dict.get(s[i+1]):
-            answer.append(Dict.get(s[i+1])- Dict.get(s[i]))
+        summed += numerals[s[i+1]] - numerals[s[i]]
+        i += 2
+print(summed)
+    
+    
