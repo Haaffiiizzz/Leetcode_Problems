37ad3967250226e4854e88ddfae33296b27d22a4
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-11-15 15:32:37 -0600

    works but is too slow. will fix tonight

--- HARD PROBLEMS/42. Trapping Rain Water.py
+++ HARD PROBLEMS/42. Trapping Rain Water.py
@@ -1,8 +1,16 @@
-height = [0,2,0,3,1,0,1,3,2,1]
-
-i = 0
-j = 0
-
-while i < len(height):
-    if height[i] == 0:
-        j = 
+class Solution:
+    def trap(height: list[int]) -> int:
+        maxRight = []
+        maxLeft = []
+        count = 0
+        for i in range(len(height)):
+            maxLeft.append(max(height[0:i]) if height[0:i] else 0)
+            maxRight.append(max(height[i:]) if height[i:] else 0)
+        
+        for i, j , k in zip(maxRight, maxLeft, height):
+            value = min(i, j) - k
+            if value > 0:
+                count += value
+        return count
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+print(Solution.trap(height))
