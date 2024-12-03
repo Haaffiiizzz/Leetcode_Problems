7253cf0077c607f904bba95d66cfdb09b3ec40b7
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-12-02 21:38:57 -0600

    Better solution

--- EASY PROBLEMS/20. Valid Parentheses.py
+++ EASY PROBLEMS/20. Valid Parentheses.py
@@ -1,18 +1,15 @@
 class Solution:
     def isValid(s: str) -> bool:
         stack = []
-        opening = ["(", "[", "{"]
-        closing = [")", "]", "}"]
         pairs = {")":"(","}":"{", "]":"["}
         for letter in s:
-                if letter in closing and pairs[letter] not in stack:
+                if letter in pairs and pairs[letter] not in stack:
                     return False
-                if letter in opening:
+                if letter in pairs.values():
                     stack.append(letter)
                 else:
                     if stack.pop() != pairs[letter]:
                         return False
-                    # stack.remove(pairs[letter])
         return not stack
 s = "()"
 print(Solution.isValid(s))
