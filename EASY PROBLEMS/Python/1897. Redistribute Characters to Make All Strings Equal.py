79e1b234b6067fc85eb54aa8a7c6b396fd3d5171
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-12-15 23:40:13 -0600

    Fixed

--- EASY PROBLEMS/Python/1897. Redistribute Characters to Make All Strings Equal.py
+++ EASY PROBLEMS/Python/1897. Redistribute Characters to Make All Strings Equal.py
@@ -1,16 +1,15 @@
 class Solution:
-    def makeEqual(self, words: List[str]) -> bool:
+    def makeEqual(words: list[str]) -> bool:
         newString = "".join(words)
-        if len(newString) % len(words) != 0:
-            return False
+        wordLen = len(words)
+        if wordLen <2:
+            return True
         if len(set(newString)) == 1:
             return True
-        if len(newString) <4:
-            return False
-        checkDict = {}
-        for i in newString:
-            if i in checkDict:
-                checkDict[i] += 1
-            else:
-                checkDict[i] = 1
-        return len(set(checkDict.values())) == 1
+        for i in set(newString):
+            if newString.count(i) % wordLen !=0:
+                return False
+        return True
+            
+words = ["bc","de"]   
+print(Solution.makeEqual(words))
