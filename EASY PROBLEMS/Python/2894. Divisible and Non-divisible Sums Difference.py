1103c07520ffa8872a424958b802a60e3fe54812
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-01-14 12:53:17 -0600

    Beats 100% runtime

---
+++ EASY PROBLEMS/Python/2894. Divisible and Non-divisible Sums Difference.py
@@ -0,0 +1,16 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        divM = 0
+        notDivM = 0
+        
+        for i in range(1, n+1):
+            if i % m == 0:
+                divM += i
+            else:
+                notDivM += i
+        
+        return notDivM-divM
+n = 10
+m = 3
+solution = Solution()
+print(solution.differenceOfSums(n,  m))
