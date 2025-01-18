26ce2863931e5d75ac99bac944b6145e037f5a44
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-01-18 05:58:58 -0600

    98% runtime.

---
+++ EASY PROBLEMS/Python/278. First Bad Version.py
@@ -0,0 +1,19 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+
+        high = n
+        low = 1
+    
+        while low <= high:
+            mid = low + (high - low ) // 2
+            if isBadVersion(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
+
+
+        
