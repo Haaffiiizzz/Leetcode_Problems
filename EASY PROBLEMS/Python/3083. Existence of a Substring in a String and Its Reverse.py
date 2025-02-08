c15ece11da7c1c10fd1bc8cec16bf2d3dd573fd4
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-02-07 23:51:06 -0600

    100% runtime

--- EASY PROBLEMS/Python/3083. Existence of a Substring in a String and Its Reverse.py
+++ EASY PROBLEMS/Python/3083. Existence of a Substring in a String and Its Reverse.py
@@ -1,26 +1,11 @@
 class Solution:
     def isSubstringPresent(self, s: str) -> bool:
         rev = s[::-1]
-        index = 0
-        
-
-        while index < len(s)- 1:
-            if s[index] == rev[index] and s[index+1] == rev[index+1]:
+        for i in range(len(s)-1):
+            if s[i:i+2] in rev:
                 return True
-            index += 1
-            
-
+                
         return False
-s = "abcd"
-t = "leetcode"
-u = "abcba"
-v = "ceo"
-rev = "edocteel"
-rev2 = "dcba"
-test = "bxthucxb"
-revTest = "bxhuctxb"
+s = "leetcode"
 solution = Solution()
 print(solution.isSubstringPresent(s))
-print(solution.isSubstringPresent(t))
-print(solution.isSubstringPresent(u))
-print(solution.isSubstringPresent(v))
