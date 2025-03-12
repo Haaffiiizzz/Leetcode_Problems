5ef0ea92f0b2bbb72e7a91e3b62434990c0e46e2
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-03-11 19:38:47 -0500

    Daily Challenge 2025-03-12

--- EASY PROBLEMS/Python/2529. Maximum Count of Positive Integer and Negative Integer.py
+++ EASY PROBLEMS/Python/2529. Maximum Count of Positive Integer and Negative Integer.py
@@ -1,11 +1,27 @@
-nums = [-2,-1,-1,1,2,3]
-pos = 0
-neg = 0       # for test
-
-for i in nums:
-    if i < 0:
-        neg += 1
-    elif i > 0:
-        pos += 1
-    
-print(pos if pos > neg else neg)        # return instead        
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        firstZero = None
+        for i in range(1, len(nums)+1):
+            if nums[i-1] == 0:
+                if firstZero == None:
+                    firstZero = i
+            elif nums[i-1] > 0:
+                if firstZero != None:
+                    neg = firstZero - 1
+                    pos = len(nums) - i + 1
+                    return max(neg, pos)
+                else:
+                    pos = len(nums) - i + 1
+                    neg = i - 1
+                    return max(pos, neg)
+        if nums[0] == 0:
+            return 0
+        if nums[-1] < 0:
+            return len(nums)
+        return len(nums) - nums.index(0)
+     
+nums = [-1764,-1562,-1226,-1216,-402,-386,-133,979,1227,1992]
+solution = Solution()
+print(solution.maximumCount(nums))
+#BEATS 100% RUNTIME
+        
