51fb3c4d97dced88c830710fa4b97b3056c5f513
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-03-17 01:58:17 -0500

    Better Solution

--- EASY PROBLEMS/Python/1636. Sort Array by Increasing Frequency.py
+++ EASY PROBLEMS/Python/1636. Sort Array by Increasing Frequency.py
@@ -1,18 +1,8 @@
+from collections import Counter
 class Solution:
     def frequencySort(self, nums: list[int]) -> list[int]:
-        numDict = {}
-        for num in nums:
-            if num in numDict:
-                numDict[num] += 1
-            else:
-                numDict[num] = 1
-        
-        keyList = sorted(numDict.keys(), key= lambda x : (numDict[x], -x))
-
-        res = []
-        for key in keyList:
-            res.extend([key] * numDict[key])
-        return res
+        numDict = Counter(nums)        
+        return sorted(nums, key= lambda x : (numDict[x], -x))
         
 nums = [1, 1, 2, 2, 2, 3]
 solution = Solution()
