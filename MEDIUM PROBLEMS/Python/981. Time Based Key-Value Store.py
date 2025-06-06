f7c03626d90f48b49fd982085b8e22efba7288ed
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-06-05 23:53:48 -0500

    still need to solve

--- MEDIUM PROBLEMS/Python/981. Time Based Key-Value Store.py
+++ MEDIUM PROBLEMS/Python/981. Time Based Key-Value Store.py
@@ -1,29 +1,25 @@
-class TimeMap:
+import bisect
 
+class TimeMap:
     def __init__(self):
+        # For each key, we store a list of tuples (timestamp, value)
         self.baseMap = {}
-        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key in self.baseMap:
-            self.baseMap[key][timestamp] = value
-        else:
-            self.baseMap[key] = {timestamp: value}
-        
-        
+        if key not in self.baseMap:
+            self.baseMap[key] = []
+        # According to problem constraints, timestamps are set in increasing order.
+        self.baseMap[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.baseMap:
             return ""
-        value = self.baseMap[key].get(timestamp,  None)
-        if value != None:
-            return value
-        minTimestamp = min(self.baseMap[key].keys())
-        while value == None and timestamp >= minTimestamp:
-            timestamp -= 1
-            value = self.baseMap[key].get(timestamp, None)
-        
-        return value if value != None else ""
+        arr = self.baseMap[key]
+        # Use bisect_right to find the insertion position and then back off one index.
+        i = bisect.bisect_right(arr, (timestamp, chr(127))) - 1
+        if i >= 0:
+            return arr[i][1]
+        return ""
             
         
 
@@ -31,4 +27,5 @@ class TimeMap:
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
+
 # param_2 = obj.get(key,timestamp)
