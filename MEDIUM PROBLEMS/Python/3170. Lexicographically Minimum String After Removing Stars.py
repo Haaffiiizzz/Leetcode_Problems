1e16fc5147a9c725d9d6fdec11b9180bb02f7f57
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-06-07 12:09:03 -0500

    Daily Challenge 2025-06-07

--- MEDIUM PROBLEMS/Python/3170. Lexicographically Minimum String After Removing Stars.py
+++ MEDIUM PROBLEMS/Python/3170. Lexicographically Minimum String After Removing Stars.py
@@ -1,20 +1,18 @@
 import heapq
 class Solution:
     def clearStars(self, s: str) -> str:
-        new = s[:]
-        storePrevSmall =[]
-        n = len(s)
-        for i in range(n): 
-            if s[i] == "*":
-                if storePrevSmall:
-                    index = heapq.heappop(storePrevSmall)
-                    print(index)
-                    new = new[0:index] + new[index+1:]
-                    new.remove("*")
-                    
+        ans = list(s)
+        heap = []
+
+        for i, char in enumerate(s):
+            if char == '*' and heap:
+                _, j = heapq.heappop(heap)
+                ans[-j] = ''
+                ans[i] = ''
             else:
-                heapq.heappush(storePrevSmall, i)
-        return new
+                heapq.heappush(heap, (char, -i))
+
+        return ''.join(ans)
                 
             
 
