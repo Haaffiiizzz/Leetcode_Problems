a1ea6e05ab42f0ac9f5b34e07c926bf0b264633b
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-07-31 23:38:47 -0500

    Daily Challenge 2025-08-01

--- EASY PROBLEMS/Python/118. Pascal's Triangle.py
+++ EASY PROBLEMS/Python/118. Pascal's Triangle.py
@@ -1,23 +1,22 @@
-rowIndex = 3    # for test
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        if numRows == 1:
+            return [[1]]
+        if numRows == 2:
+            return [[1], [1, 1]]
+        i = 2
+        final = [[1],[1, 1]]
+        while i < numRows:
+            rowList = [1,1]
+            for j in range(1, i):
+                prev = final[i-1]
+                left, right = prev[j-1], prev[j]
+                total = left + right
+                rowList.insert(j, total)
+            final.append(rowList)
+            i += 1
 
-
-if rowIndex == 0:
-    print([1])    # return instead
-    quit()
-if rowIndex == 1:
-    print([1, 1])   # return instead
-    quit()
-
-i = 2
-final = [[1],[1, 1]]
-while i <= rowIndex:
-    rowList = [1,1]
-    for j in range(1, i):
-        prev = final[i-1]
-        left, right = prev[j-1], prev[j]
-        total = left + right
-        rowList.insert(j, total)
-    final.append(rowList)
-    i += 1
-
-print(final[rowIndex])    # return instead
+        return final
+numRows = 5
+solution = Solution()
+print(solution.generate(numRows))
