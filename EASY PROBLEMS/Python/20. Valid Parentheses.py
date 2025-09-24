a4cea87643fb41aac64168057c5d4373e80b9dd7
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-09-24 13:41:54 -0500

    better solution

--- EASY PROBLEMS/Python/20. Valid Parentheses.py
+++ EASY PROBLEMS/Python/20. Valid Parentheses.py
@@ -3,13 +3,21 @@ class Solution:
         stack = []
         pairs = {")":"(","}":"{", "]":"["}
         for letter in s:
-                if letter in pairs and pairs[letter] not in stack:
-                    return False
                 if letter in pairs.values():
-                    stack.append(letter)
+                     stack.append(letter)
                 else:
-                    if stack.pop() != pairs[letter]:
+                    if not stack:
                         return False
+                    last = stack.pop()
+                    if pairs[letter] != last:
+                        return False
+                # if letter in pairs and pairs[letter] not in stack:
+                #     return False
+                # if letter in pairs.values():
+                #     stack.append(letter)
+                # else:
+                #     if stack.pop() != pairs[letter]:
+                #         return False
         return not stack
 s = "()"
 print(Solution.isValid(s))
