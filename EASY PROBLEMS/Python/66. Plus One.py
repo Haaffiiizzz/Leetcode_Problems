ede99235d597da9f73996889c322e285eda998de
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2026-01-15 23:26:42 -0600

    Easy one liner

--- EASY PROBLEMS/Python/66. Plus One.py
+++ EASY PROBLEMS/Python/66. Plus One.py
@@ -1,14 +1,6 @@
-digits = [1,2,3]    # test
-for i in range(len(digits)):
-        digits[i] = str(digits[i])
-
-sum = ""
-for i in digits:
-    sum += i
-
-sum = str(int(sum) + 1)
-final = []
-for i in range(len(sum)):
-    final.append(int(sum[i]))
-
-print(final)      #return instead  
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        return list(map(int, str(int("".join(map(str, digits))) + 1)))
+digits = [1,2,3]
+solution = Solution()
+print(solution.plusOne(digits))
